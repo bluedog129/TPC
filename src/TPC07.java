8f2adb83df0ade6d718b2b73d6489f7fb234dae7
bluedog129 <bluedog129@gmail.com>
2021-06-30 00:32:29 +0900

    매개변수 전달기법 주석 추가

--- src/TPC07.java
+++ src/TPC07.java
@@ -2,29 +2,37 @@
 public class TPC07 {
 
 	public static void main(String[] args) {
+		// Call By Value
 		int a=20;
 		float b = 56.7f;
 		// a+b =?
-		float v = sum(a,b);  // Call By Value
+		float v = sum(a,b);  
 		System.out.println(v); //76.7s
 		
+		// ------------------------------------
+		
+		//Call By Reference
 		int[] arr = {1,2,3,4,5};
-		// 배열의 총합?
+		// 배열의 총합구하기
 		int vv = arrSum(arr);
 		System.out.println(vv);
 	}
 	
-	private static int arrSum(int[] x) {
-		int sum = 0;
-		for(int i=0; i<x.length; i++) {
-			sum += x[i];
-		}
-		
-		return sum;
-	}
-	
+	// Call By Value(값 전달 기법) : 변수안의 값을 매서드의 매개변수로 전달하는 기법, 기억공간 개별
+	// 메소드 호출이 가능한조건
+	// 실인수와 가인수의 개수, DataType이 같아야함
 	public static float sum(int a, float b) {
 		float v =a+b;
 		return v;
 	}
+	
+	// Call By Reference(번지 전달 기법) : 변수안에 저장되어있는 해당배열의 메모리 번지값을 전달하는 기법, 기억공간 공유 
+		private static int arrSum(int[] x) {
+			int sum = 0;
+			for(int i=0; i<x.length; i++) {
+				sum += x[i];
+			}
+			
+			return sum;
+		}
 }
