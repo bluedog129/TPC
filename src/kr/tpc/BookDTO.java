ae4de73e38304f04498cf86367aabe7ccf1607ee
bluedog129 <bluedog129@gmail.com>
2021-06-30 23:47:00 +0900

    객체의 생성과정

--- src/kr/tpc/BookDTO.java
+++ src/kr/tpc/BookDTO.java
@@ -8,10 +8,10 @@ public class BookDTO {
 	
 	// 디폴트 생성자 매서드 (원래 생략되어 있음)
 	public BookDTO() {
-		// 객체를 생성하는 작업을 한다.
+		// 객체를 생성하는 작업을 한다. (heap영역에 생성, 기계어코드)
 		
 	}
-
+	// 객체가 heap영역에 생성됨과 동시에 this 라는 객체가 포인터로 해당객체를 가리킨다.
 	public BookDTO(String title, int price, String company, int page) {
 		this.title = title;
 		this.price = price;
